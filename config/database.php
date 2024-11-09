717387608da9c1c2a6bc35e44082f3f3ab9869ff
anikmondol <anikmondol558363@gmail.com>
2024-11-09 19:06:56 +0600

    Start Libarary Management System database connection successfully done.

---
+++ config/database.php
@@ -0,0 +1,14 @@
+<?php
+
+// include_once("config/config.php");
+
+$conn = @new mysqli(SERVER_NAME, USERNAME, PASSWORD, DATABASE);
+
+// check connection
+
+if ($conn->connect_error) {
+    die("connection failed: ". $conn->connect_error);
+}
+
+
+?>
