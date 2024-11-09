293abfb86c97b9a554720ed8645fff5b37ad2362
anikmondol <anikmondol558363@gmail.com>
2024-11-09 12:12:09 +0600

    Start Libarary Management System index, forget, reset, and dashebord page  HTML to PHP conversion done.

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+
+<html lang="en" data-bs-theme="light" data-menu-color="brand" data-topbar-color="light">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+
+
+<head>
+  <meta charset="utf-8" />
+  <title> Login | Start Library Management System </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+  <meta content="Anik Mondal" name="author" />
+
+  <!-- App favicon -->
+  <link rel="shortcut icon" href="assets/images/images/neptune.png">
+
+  <link href="assets/libs/morris.js/morris.css" rel="stylesheet" type="text/css" />
+
+  <!-- App css -->
+  <link href="assets/css/style.min.css" rel="stylesheet" type="text/css">
+  <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css">
+  <script src="assets/js/config.js"></script>
+
+  <!-- bootstrap css  -->
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+  <!-- Custom style -->
+  <link rel="stylesheet" href="assets/css/style.css">
+
+
+</head>
+
+<body class="body-background">
+  <!-- main content start -->
+  <section class="container d-flex align-items-center justify-content-center vh-100">
+    <div class="row">
+      <div class="col-md-12 login-form">
+        <div class="card mb-3" style="max-width: 900px">
+          <div class="row g-0">
+            <div class="col-md-5">
+              <img src="./assets/images/images/login-bg.jpg" class="img-fluid rounded-start h-100" />
+            </div>
+            <div class="col-md-7">
+              <div class="card-body">
+                <h1 class="card-title text-uppercase fw-bold">
+                  star library
+                </h1>
+                <p class="card-text">Enter email and password to login</p>
+                <form action="./dashboard.php">
+                  <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" />
+                  </div>
+                  <button type="submit" class="btn btn-primary">Login</button>
+                </form>
+                <hr />
+                <a href="./forgot-password.php" class="card-text text-center link-underline-light">Forgot Password?</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- main content end -->
+
+  <!-- App js -->
+  <script src="assets/js/vendor.min.js"></script>
+  <script src="assets/js/app.js"></script>
+
+  <!-- Knob charts js -->
+  <script src="assets/libs/jquery-knob/jquery.knob.min.js"></script>
+
+  <!-- Sparkline Js-->
+  <script src="assets/libs/jquery-sparkline/jquery.sparkline.min.js"></script>
+
+  <script src="assets/libs/morris.js/morris.min.js"></script>
+
+  <script src="assets/libs/raphael/raphael.min.js"></script>
+
+  <!-- Dashboard init-->
+  <script src="assets/js/pages/dashboard.js"></script>
+
+</body>
+
+
+
+</html>
---
+++ reset-password.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+
+<html lang="en" data-bs-theme="light" data-menu-color="brand" data-topbar-color="light">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+
+
+<head>
+    <meta charset="utf-8" />
+    <title> Reset Password | Start Library Management System </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+    <meta content="Anik Mondal" name="author" />
+
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="assets/images/images/neptune.png">
+
+    <link href="assets/libs/morris.js/morris.css" rel="stylesheet" type="text/css" />
+
+    <!-- App css -->
+    <link href="assets/css/style.min.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css">
+    <script src="assets/js/config.js"></script>
+
+    <!-- bootstrap css  -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+    <!-- Custom style -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+
+</head>
+
+<body class="body-background">
+
+    <!-- main content start -->
+    <section class="container d-flex align-items-center justify-content-center vh-100">
+        <div class="row">
+            <div class="col-md-12 login-form">
+                <div class="card mb-3" style="max-width: 900px;">
+                    <div class="row g-0">
+                        <div class="col-md-5">
+                            <img src="assets/images/images/login-bg.jpg" class="img-fluid rounded-start h-100"
+                                alt="Start Library Image Adding Son">
+                        </div>
+                        <div class="col-md-7">
+                            <div class="card-body">
+                                <h1 class="card-title text-uppercase fw-bold">Start Library</h1>
+                                <p class="card-text"> Reset Password</p>
+                                <form action="./reset-password.php">
+                                    <div class="mb-3">
+                                        <label for="exampleInputEmail1" class="form-label">Reset Password Code</label>
+                                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="exampleInputEmail1" class="form-label">New Password</label>
+                                        <input type="password" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="exampleInputEmail1" class="form-label">Confirm Password</label>
+                                        <input type="password" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </form>
+                                <hr>
+                                <a href="./index.php" class="card-link text-decoration-none">Login?</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- main content end -->
+
+
+    <!-- App js -->
+    <script src="assets/js/vendor.min.js"></script>
+    <script src="assets/js/app.js"></script>
+
+    <!-- Knob charts js -->
+    <script src="assets/libs/jquery-knob/jquery.knob.min.js"></script>
+
+    <!-- Sparkline Js-->
+    <script src="assets/libs/jquery-sparkline/jquery.sparkline.min.js"></script>
+
+    <script src="assets/libs/morris.js/morris.min.js"></script>
+
+    <script src="assets/libs/raphael/raphael.min.js"></script>
+
+    <!-- Dashboard init-->
+    <script src="assets/js/pages/dashboard.js"></script>
+
+</body>
+
+
+
+</html>
